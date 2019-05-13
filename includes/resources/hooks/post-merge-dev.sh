dfb76058bdc2a2701d5a163eee5df444bbe40811
Milos <m@deva.co>
2019-05-13 10:22:09 +0200

    Fix the git post merge hook.

--- includes/resources/hooks/post-merge-dev.sh
+++ includes/resources/hooks/post-merge-dev.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 GIT_DIR='.git'
 composer install && \
+    php artisan queue:restart && \
     php artisan cache:clear && \
     php artisan route:clear && \
+    php artisan view:clear && \
     php artisan config:clear
 
+# if run from the shell
 read -rsp $'Press any key to start compiling assets. To abort press <CTRL+C>.\n' -n1 key
 
-yarn
-yarn run development
+# skip compiling assets if `FASTER_MERGE=no git pull` is run
+if [ "$FASTER_MERGE" != "no" ]; then
+    yarn
+    yarn run development
+fi
--- includes/resources/hooks/post-merge-prod.sh
+++ includes/resources/hooks/post-merge-prod.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 GIT_DIR='.git'
 composer install && \
+    php artisan queue:restart && \
     php artisan cache:clear && \
-    php artisan config:cache && \
-    php artisan route:cache
+    php artisan route:clear && \
+    php artisan view:clear && \
+    php artisan config:clear
 
-sudo pm2 restart -1
-
+# if run from the shell
 read -rsp $'Press any key to start compiling assets. To abort press <CTRL+C>.\n' -n1 key
 
-yarn
-yarn run production
+# skip compiling assets if `FASTER_MERGE=no git pull` is run
+if [ "$FASTER_MERGE" != "no" ]; then
+    yarn
+    yarn run production
+fi
